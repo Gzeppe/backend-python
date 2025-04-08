f26209d3a5dfca3bc739ba7bb56c81ce8957e3cb
gzeppe <giulianog54@gmail.com>
2025-04-08 00:22:43 -0400

    image and text upload program

--- files/forms.py
+++ files/forms.py
@@ -5,8 +5,7 @@ from .models import File
 class UploadForm(ModelForm):
     class Meta:
         model = File
-        fields = ['name', 'file_type', 'file']  # Include file_type here
+        fields = ['name', 'file', 'file_type'] 
         widgets = {
-            'name': forms.TextInput(attrs={'placeholder': 'filename'}),
-            'file_type': forms.TextInput(attrs={'placeholder': 'file type'})  # Optional: Add a placeholder for file_type
+            'name': forms.TextInput(attrs={'placeholder': 'filename'})
         }
